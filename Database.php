f1154a5ec87baebe9283bd83175a8f8dc2a2c28a
mwaura69 <74621732+mwaura69@users.noreply.github.com>
2023-02-12 15:27:02 +0300

    Update Database.php

--- Database.php
+++ Database.php
@@ -3,7 +3,7 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpassword = "";
-$dbname = "myproject_db";
+$dbname = #yourDataBaseGoesHere ;
 
 
 if(!$conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname))
